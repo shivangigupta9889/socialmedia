263f5baf3b3d368a36ae4230ac1eb69867a9b25c
Shivam Gupta <shivamguptadrps210001@gmail.com>
2023-08-14 11:18:02 +0530

    update App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -5,17 +5,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./src/component/About";
 import Contact from "./src/component/Contact";
 import JoinPage from "./src/component/JoinPage";
+import Homepage from "./src/component/HomePage";
 
 const AppLayout=()=>{
     return (<div>
         {/* <Header/> */}
+        
         <JoinPage/>
-
+        
         </div>)
 };
 const appRouter = createBrowserRouter([
   
     {path: "/",element: <AppLayout />},
+    {path: "/main",element: <Homepage />},
     {path: "/about", element: <About />} ,
     {path: "/contact", element: <Contact /> ,
   }
